f9741e7d708d5c6c1e1d752e18cd7699464b5291
Ryan Preston <r.preston73@gmail.com>
2022-11-02 16:27:21 -0400

    feat: take input file, gen output file

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+#[macro_use]
+extern crate log;
+
+use std::str::from_utf8;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author = "Ryan P.", version, about)]
+/// Generates a json file containing a list
+struct Args {
+    /// Contains a file with list entries on each line
+    /// Example:
+    /// 1. foo
+    /// 2. bar
+    /// 3. buzz
+    #[arg(short, long = "input-file")]
+    input_file: String,
+
+    /// The output file name
+    #[arg(short, long = "output-file")]
+    output_file: String,
+}
+
+fn main() {
+    pretty_env_logger::init();
+
+    let args = Args::parse();
+
+    let raw_input = std::fs::read(&args.input_file).expect("Unable to read input file");
+    let input = from_utf8(&raw_input).unwrap();
+
+    let list: Vec<&str> = input.split_terminator("\n").collect();
+    let json_list = serde_json::to_string_pretty(&list).unwrap();
+
+    std::fs::write(&args.output_file, &json_list).expect("Unable to write to file");
+    info!("Your file has been created at {}", &args.output_file);
+}
